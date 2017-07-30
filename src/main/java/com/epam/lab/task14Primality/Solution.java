27da278f874c2af06530e8ebe0040863d2a64a61
Valeriy Burmistrov <chrisburrrma@gmail.com>
2017-07-31 02:04:38 +0300

    task14 done with ugly sqrt(n) loop. yes i know about eratosthenes

---
+++ src/main/java/com/epam/lab/task14Primality/Solution.java
@@ -0,0 +1,26 @@
+package com.epam.lab.task14Primality;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int p = in.nextInt();
+        for (int a0 = 0; a0 < p; a0++) {
+            int n = in.nextInt();
+            System.out.println(isPrime(n) ? "Prime" : "Not prime");
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return n > 1;
+    }
+}
